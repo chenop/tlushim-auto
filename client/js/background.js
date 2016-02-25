65e720879442cf1e50771c55e3faa4ff68b32d2d
Chen <chenop@gmail.com>
2016-02-25 07:43:37 +0200

    - Adding alarmi

--- client/js/background.js
+++ client/js/background.js
@@ -3,3 +3,18 @@
  */
 
 console.log('background is here...');
+
+var date = new Date();
+var enterDateInMilisec = date.setHours(9);
+
+
+chrome.alarms.create("enterAlarm", {
+    'when': enterDateInMilisec
+    , 'delayInMinutes': null
+    , 'periodInMinutes' : 1 //24 * 60 // daily
+})
+
+chrome.alarms.onAlarm.addListener(function() {
+    console.log('alarm!');
+})
+
