1fa90d10fec9fdc0880168ebe57fd1cd27608dcf
Chen <chenop@gmail.com>
2016-02-27 23:22:21 +0200

    - tlushimApi - implementing...

--- client/js/tlushimApi.js
+++ client/js/tlushimApi.js
@@ -15,7 +15,7 @@ angular.module('tlushim-auto')
             console.log("exit tlusim with: ", userData);
         }
 
-        public.login = function(userData) {
+        function tlushimLogin() {
             if (!userData.idNum || !userData.password)
                 console.log('userData is not valid - missing idNum or password fields', userData);
 
@@ -24,37 +24,52 @@ angular.module('tlushim-auto')
             formData.append('password', userData.password);
             formData.append('connect', "התחברות");
 
-            $http({
+            return $http({
                 method: 'POST',
                 url: 'https://www.tlushim.co.il/login.php',
                 data: formData,
                 headers: { 'Content-Type': undefined },
                 transformRequest: angular.identity,
                 //headers: {'Content-Type': 'multipart/form-data'}  // set the headers so angular passing info as form data (not request payload)
-            }).then(function (result) {
-                console.log(result);
             })
         }
 
+        function clockIn() {
+            var formData = new FormData();
+
+            return $http({
+                method: 'POST',
+                url: 'https://www.tlushim.co.il/main.php?op=post_clock',
+                data: formData,
+                headers: { 'Content-Type': undefined },
+                transformRequest: angular.identity,
+            })
+        }
+
+        function clockOut() {
+            var formData = new FormData();
+
+            return $http({
+                method: 'POST',
+                url: 'https://www.tlushim.co.il/main.php?op=post_clock', // TODO chen set the right Url
+                data: formData,
+                headers: { 'Content-Type': undefined },
+                transformRequest: angular.identity,
+            })
+        }
+
+        public.login = function(userData) {
+            tlushimLogin(userData)
+                .then(function (result) {
+                    return clockIn();
+                })
+        }
+
         public.logout = function(userData) {
-            //if (!userData.idNum || !userData.password)
-            //    console.log('userData is not valid - missing idNum or password fields', userData);
-            //
-            //var formData = new FormData();
-            //formData.append('id_num', userData.idNum);
-            //formData.append('password', userData.password);
-            //formData.append('connect', "התחברות");
-            //
-            //$http({
-            //    method: 'POST',
-            //    url: 'https://www.tlushim.co.il/login.php',
-            //    data: formData,
-            //    headers: { 'Content-Type': undefined },
-            //    transformRequest: angular.identity,
-            //    //headers: {'Content-Type': 'multipart/form-data'}  // set the headers so angular passing info as form data (not request payload)
-            //}).then(function (result) {
-            //    console.log(result);
-            //})
+            tlushimLogin(userData)
+                .then(function (result) {
+                    return clockOut();
+                })
         }
 
         return public;
